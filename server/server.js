f9a4b9b4617cdb9242c3b7f50ee07594763105db
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2022-06-28 15:44:03 +0200

    refractor and add error handeling route

--- server/server.js
+++ server/server.js
@@ -1,27 +1,31 @@
 require("dotenv").config();
 
-const morgan = require("morgan");
-
-const cors = require("cors");
-
 const express = require("express");
 
-const app = express();
+//logging middleware
+const morgan = require("morgan");
 
+//cross origin middleware
+const cors = require("cors");
+
+// masjid router file
+const masjidRoute = require("./routes/masjid.js");
+const { errorHandler } = require("./helpers/error.js");
+
+const app = express();
 const apiRouter = express.Router();
 
-const masjidRoute = require("./routes/masjid.js");
-
-const port = process.env.PORT;
-
 app.use(cors());
+apiRouter.use(morgan("dev"));
 
 app.use("/api/v1/",apiRouter)
 
-apiRouter.use(morgan("dev"));
-
+// Parse JSON in request body
 apiRouter.use(express.json())
 
-apiRouter.use("/masjid" , masjidRoute)
+apiRouter.use("/masjid" , masjidRoute);
 
+app.use(errorHandler);
+
+const port = process.env.PORT;
 app.listen(port , ()=>{console.log(`Listening on port ${port}`)});
